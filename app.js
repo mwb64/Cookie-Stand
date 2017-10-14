dae090ffcdb589400d81d37e2e3e02af3cfaf616
Michael Brown <mbrown488@gmail.com>
2017-10-14 16:42:06 -0700

    Created Object Constructors

--- app.js
+++ app.js
@@ -2,40 +2,39 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-var cookieShop1 = {
-  location: '1st & Pike',
-  minCust: 23,
-  maxCust: 65,
-  avgCookieSale: 6.3,
-  avgCustPerHour: [],
-  cookieSoldPH: [],
-  randAvgCustPerHour: function(){
+var allLocations = [];
+var cookieSoldPH = 0;
+var cookieSoldPD = 0;
+
+function store (location,minCust,maxCust,avgCookeSales){
+  this.methodName = function () {
+
+  };location = location;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookieSales = avgCookeSales;
+  this.avgCustPerHour = [];
+  this.cookieSoldPH = [];
+  this.totalCookies = 0;
+  allLocations.push(this);
+  this.randAvgCustPerHour = function(){
     for(var i = 0;i < hours.length; i++){
       this.avgCustPerHour.push(Math.floor((Math.random() * (this.maxCust - this.minCust ) + 1)));
-      console.log(this.avgCustPerHour[i]);
-    }
-  },
-  randAvgCkePerHour: function(){
+    };
+  };
+  this.randAvgCustPerHour();
+  this.randAvgCkePerHour = function(){
     for(var j = 0; j < hours.length;j++){
       this.cookieSoldPH.push(Math.round(this.avgCookieSale * this.avgCustPerHour[j]));
-      console.log(this.randAvgCkePerHour);
-    }
-  },
-  renderFunct: function(){
-    var store1 = document.getElementById('store1');
-    var firstandpike = document.getElementById('firstandpike');
-    this.randAvgCustPerHour();
-    this.randAvgCkePerHour();
-    var h3El = document.createElement('h3');
-    h3El.textContent = this.location;
-    store1.appendChild(h3El);
-    for(var k = 0; k < hours.length; k++){
-      var liEl = document.createElement('li');
-      liEl.textContent = hours[k] + ': ' + this.cookieSoldPH[k] + ' cookies';
-      console.log(liEl);
-      firstandpike.appendChild(liEl);
-    }
-  }
-};
+    };
+  };
+  this.randAvgCkePerHour();
+}
 
-cookieShop1.renderFunct();
+function makeLocations(){
+  new store('SeaTac Airport',3,24,6.3);
+  new store('Seattle Center',11,38,3.7);
+  new store('Capitol Hill',20,38,2.3);
+  new store('Alki',2,16,4.6);
+};
+makeLocations();
